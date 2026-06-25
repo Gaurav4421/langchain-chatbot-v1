3b53a7df23edd8ddb85a83d19972d43ae9cd6919
Gaurav4421 <gv900118@gmail.com>
2026-06-25 17:40:58 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,24 +5,15 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import ChatMessageHistory
 
-# -----------------------------
-# Load Environment Variables
-# -----------------------------
 load_dotenv()
 
 api_key = os.getenv("GROQ_API_KEY")
 
-# -----------------------------
-# Initialize Model
-# -----------------------------
 model = ChatGroq(
     model="llama-3.3-70b-versatile",
     api_key=api_key
 )
 
-# -----------------------------
-# Create Prompt
-# -----------------------------
 prompt = ChatPromptTemplate.from_messages([
     (
         "system",
@@ -37,9 +28,6 @@ prompt = ChatPromptTemplate.from_messages([
     )
 ])
 
-# -----------------------------
-# Initialize Chat History
-# -----------------------------
 history = ChatMessageHistory()
 
 print("=" * 60)
@@ -47,9 +35,6 @@ print("🤖 AI Chatbot")
 print("Type 'exit' to quit")
 print("=" * 60)
 
-# -----------------------------
-# Chat Loop
-# -----------------------------
 while True:
 
     user_input = input("\nYou : ")
@@ -58,18 +43,14 @@ while True:
         print("\nGoodbye 👋")
         break
 
-    # Create Prompt
     final_prompt = prompt.invoke({
         "chat_history": history.messages,
         "question": user_input
     })
 
-    # Get Response
     response = model.invoke(final_prompt)
 
-    # Display Response
     print("\nAI :", response.content)
 
-    # Store Conversation
     history.add_user_message(user_input)
     history.add_ai_message(response.content)
